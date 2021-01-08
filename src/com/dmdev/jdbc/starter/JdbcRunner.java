89b6fe5dd5376b918a4bccc25e7a66e155498110
Dzianis Matveyenka <dmatveyenka@fitbit.com>
2021-01-08 17:15:59 +0300

    added files from lesson-5

--- src/com/dmdev/jdbc/starter/JdbcRunner.java
+++ src/com/dmdev/jdbc/starter/JdbcRunner.java
@@ -9,8 +9,18 @@ public class JdbcRunner {
 
     public static void main(String[] args) throws SQLException {
         Class<Driver> driverClass = Driver.class;
-        try (var connection = ConnectionManager.open()) {
+        String sql = """
+                CREATE TABLE IF NOT EXISTS info (
+                    id SERIAL PRIMARY KEY ,
+                    data TEXT NOT NULL 
+                );
+                """;
+        try (var connection = ConnectionManager.open();
+             var statement = connection.createStatement()) {
+            System.out.println(connection.getSchema());
             System.out.println(connection.getTransactionIsolation());
+            var executeResult = statement.execute(sql);
+            System.out.println(executeResult);
         }
     }
 }
