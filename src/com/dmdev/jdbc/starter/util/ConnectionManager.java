e59a6b688d9ae7c335c45468e326efd04482b656
Dzianis Matveyenka <dmatveyenka@fitbit.com>
2021-01-08 16:38:59 +0300

    added files from lesson-4

--- src/com/dmdev/jdbc/starter/util/ConnectionManager.java
+++ src/com/dmdev/jdbc/starter/util/ConnectionManager.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 
 public final class ConnectionManager {
 
-    private static final String PASSWORD = "postgres";
-    private static final String USERNAME = "postgres";
-    private static final String URL = "jdbc:postgresql://localhost:5432/flight_repository";
+    private static final String PASSWORD_KEY = "db.password";
+    private static final String USERNAME_KEY = "db.username";
+    private static final String URL_KEY = "db.url";
 
     static {
         loadDriver();
@@ -19,7 +19,11 @@ public final class ConnectionManager {
 
     public static Connection open() {
         try {
-            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            return DriverManager.getConnection(
+                    PropertiesUtil.get(URL_KEY),
+                    PropertiesUtil.get(USERNAME_KEY),
+                    PropertiesUtil.get(PASSWORD_KEY)
+            );
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
---
+++ src/com/dmdev/jdbc/starter/util/PropertiesUtil.java
@@ -0,0 +1,28 @@
+package com.dmdev.jdbc.starter.util;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public final class PropertiesUtil {
+
+    private static final Properties PROPERTIES = new Properties();
+
+    static {
+        loadProperties();
+    }
+
+    private PropertiesUtil() {
+    }
+
+    public static String get(String key) {
+        return PROPERTIES.getProperty(key);
+    }
+
+    private static void loadProperties() {
+        try (var inputStream = PropertiesUtil.class.getClassLoader().getResourceAsStream("application.properties")) {
+            PROPERTIES.load(inputStream);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
