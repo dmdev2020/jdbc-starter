9cbc58eb2f80194ef5845354d8c7297b80d4c340
Dzianis Matveyenka <dmatveyenka@fitbit.com>
2021-01-08 14:00:01 +0300

    added files from lesson-2

---
+++ src/com/dmdev/jdbc/starter/JdbcRunner.java
@@ -0,0 +1,10 @@
+package com.dmdev.jdbc.starter;
+
+import org.postgresql.Driver;
+
+public class JdbcRunner {
+
+    public static void main(String[] args) {
+        Class<Driver> driverClass = Driver.class;
+    }
+}
