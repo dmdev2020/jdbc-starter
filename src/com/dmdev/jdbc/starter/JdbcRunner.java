67784f99c426a976d194838091c35c44936db275
Dzianis Matveyenka <dmatveyenka@fitbit.com>
2021-01-09 19:54:08 +0300

    added files from lesson-7

--- src/com/dmdev/jdbc/starter/JdbcRunner.java
+++ src/com/dmdev/jdbc/starter/JdbcRunner.java
@@ -3,6 +3,7 @@ package com.dmdev.jdbc.starter;
 import com.dmdev.jdbc.starter.util.ConnectionManager;
 import org.postgresql.Driver;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class JdbcRunner {
@@ -10,18 +11,23 @@ public class JdbcRunner {
     public static void main(String[] args) throws SQLException {
         Class<Driver> driverClass = Driver.class;
         String sql = """
-                UPDATE info
-                SET data = 'TestTest'
-                WHERE id = 5
-                RETURNING *
+                SELECT *
+                FROM ticket
                 """;
         try (var connection = ConnectionManager.open();
-             var statement = connection.createStatement()) {
+             var statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE)) {
             System.out.println(connection.getSchema());
             System.out.println(connection.getTransactionIsolation());
-            var executeResult = statement.execute(sql);
-            System.out.println(executeResult);
-            System.out.println(statement.getUpdateCount());
+
+            var executeResult = statement.executeQuery(sql);
+            while (executeResult.next()) {
+                System.out.println(executeResult.getLong("id"));
+                System.out.println(executeResult.getString("passenger_no"));
+                System.out.println(executeResult.getBigDecimal("cost"));
+//                executeResult.updateLong("id", 1000);
+//                executeResult.las
+                System.out.println("------");
+            }
         }
     }
 }
