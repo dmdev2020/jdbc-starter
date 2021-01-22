0d0b8536fe9961eb3ec74c1e05b71e4f45abd308
Dzianis Matveyenka <dmatveyenka@fitbit.com>
2021-01-22 16:52:19 +0300

    added files from lesson-14

--- src/com/dmdev/jdbc/starter/TransactionRunner.java
+++ src/com/dmdev/jdbc/starter/TransactionRunner.java
@@ -3,33 +3,29 @@ package com.dmdev.jdbc.starter;
 import com.dmdev.jdbc.starter.util.ConnectionManager;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class TransactionRunner {
 
     public static void main(String[] args) throws SQLException {
         long flightId = 8;
-        var deleteFlightSql = "DELETE FROM flight WHERE id = ?";
-        var deleteTicketsSql = "DELETE FROM ticket WHERE flight_id = ?";
+        var deleteFlightSql = "DELETE FROM flight WHERE id = " + flightId;
+        var deleteTicketsSql = "DELETE FROM ticket WHERE flight_id = " + flightId;
+        var createTableSql = "CREATE TABLE test4 (id SERIAL PRIMARY KEY);";
         Connection connection = null;
-        PreparedStatement deleteFlightStatement = null;
-        PreparedStatement deleteTicketsStatement = null;
+        Statement statement = null;
         try {
             connection = ConnectionManager.open();
-            deleteFlightStatement = connection.prepareStatement(deleteFlightSql);
-            deleteTicketsStatement = connection.prepareStatement(deleteTicketsSql);
-
             connection.setAutoCommit(false);
 
-            deleteFlightStatement.setLong(1, flightId);
-            deleteTicketsStatement.setLong(1, flightId);
+            statement = connection.createStatement();
+            statement.addBatch(deleteTicketsSql);
+            statement.addBatch(deleteFlightSql);
+            statement.addBatch(createTableSql);
 
-            deleteTicketsStatement.executeUpdate();
-            if (true) {
-                throw new RuntimeException("Ooops");
-            }
-            deleteFlightStatement.executeUpdate();
+            var ints = statement.executeBatch();
+
             connection.commit();
         } catch (Exception e) {
             if (connection != null) {
@@ -40,11 +36,8 @@ public class TransactionRunner {
             if (connection != null) {
                 connection.close();
             }
-            if (deleteFlightStatement != null) {
-                deleteFlightStatement.close();
-            }
-            if (deleteTicketsStatement != null) {
-                deleteTicketsStatement.close();
+            if (statement != null) {
+                statement.close();
             }
         }
     }
